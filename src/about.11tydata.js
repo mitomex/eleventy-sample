7480d100293b838170524e5d1c6e2579165afc3d
Yusuke Mitome <info@mitomex.jp>
2021-01-04 00:25:51 +0900

    About ページのデータを JSON から JS に変更

---
+++ src/about.11tydata.js
@@ -0,0 +1,14 @@
+const baseAccount = "mitomex";
+
+module.exports = {
+  medias: [
+    {
+      name: "Twitter",
+      url: `https://www.twitter.com/${baseAccount}`
+    },
+    {
+      name: "Instagram",
+      url: `https://www.instagram.com/${baseAccount}`
+    }
+  ]
+};
