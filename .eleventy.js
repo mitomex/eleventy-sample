433c7c5cb1c3279ecc743fcb46514447985faf04
Yusuke Mitome <info@mitomex.jp>
2021-01-05 10:10:40 +0900

    読みやすい日付に変換するフィルターを追加した

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,13 @@ module.exports = function(eleventyConfig) {
     return dateObj.getFullYear();
   });
 
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    let YYYY = dateObj.getFullYear();
+    let M = dateObj.getMonth() + 1;
+    let D = dateObj.getDate();
+    return `${YYYY}年${M}月${D}日`;
+  });
+
   return {
     dir: {
       input: "src",
