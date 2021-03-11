3453414896b56fa19c5e8b3e63f487d51677e9b8
Yusuke Mitome <info@mitomex.jp>
2021-03-11 09:43:28 +0900

    タイトルを変更

--- src/about.11tydata.js
+++ src/about.11tydata.js
@@ -4,7 +4,7 @@ module.exports = () => {
   return {
     medias: [
       {
-        name: "Twitter",
+        name: "Twitter ID",
         url: `https://www.twitter.com/${baseAccount}`
       },
       {
